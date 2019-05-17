06afd55732066ca83f1d922788ee24a5e3bc9145
null <null@pop-os.localdomain>
2019-05-17 14:14:32 -0300

    fix2

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ sudo apt install -y docker.io
 sudo usermod -a -G docker $USER
 sudo cp ./changeip /usr/local/bin/changeip
 sudo chmod +x /usr/local/bin/changeip
-su - $USER
-docker build -t torproxy .
-docker run --restart unless-stopped -d -p 8118:8118 -p 9050:9050 torproxy
+sudo docker build -t torproxy .
+sudo docker run --restart unless-stopped -d -p 8118:8118 -p 9050:9050 torproxy
